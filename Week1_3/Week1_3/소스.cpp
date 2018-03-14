c4ec29a5bbf88dcc8659cfebfb24fc5e65b112e8
dsaf2007 <dsaf2007@naver.com>
2018-03-14 16:58:26 +0900

    class complete

--- Week1_3/Week1_3/소스.cpp
+++ Week1_3/Week1_3/소스.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
 
+typedef double itemType;
 
-class
+class Stack2
+{
+public:
+	Stack2()
+	{
+		head = new node;
+		head->next = NULL;
+	}
+	~Stack2()
+	{
+
+	}
+	void push(itemType v)
+	{
+		struct node *temp = new node;
+		temp->key = v;
+		temp->next = head->next;
+		head->next = temp;
+	}
+	itemType pop()
+	{
+		struct node *temp = head->next;
+		itemType v = temp->key;
+		head->next = temp->next;
+		free(temp);
+		return v;
+	}
+	int empty()
+	{
+		if (head->next == NULL)
+			return 1;
+	}
+
+private:
+	struct node
+	{
+		itemType key; struct node *next;
+	};
+	struct node *head, *z;
+};
+
+
+void main()
+{
+
+}
